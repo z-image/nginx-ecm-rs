655cfd3f66be05ed740fceb2b21aa447d0b67fc7
Teodor Milkov <zimage@icdsoft.com>
2021-10-20 12:27:49 +0300

    v0.0.2: PoC -> beta

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
 #![allow(non_camel_case_types)]
+#![deny(clippy::all)]
+#![deny(clippy::pedantic)]
 
+use std::convert::TryFrom;
+use std::error;
+use std::fmt;
 use std::fs;
 use std::io::prelude::*;
 use std::mem;
 use std::path::Path;
 use std::slice;
+use std::str;
+use std::time::SystemTime;
 
+use std::sync::atomic::{AtomicBool, Ordering};
 use structopt::StructOpt;
+
+extern crate chrono;
+use chrono::prelude::*;
+
+extern crate chrono_english;
+use chrono_english::{parse_date_string, Dialect};
 
 #[macro_use]
 extern crate lazy_static;
 
-const NGX_FC_HEADER_VERSION: ngx_uint_t = 5;
+// Thread-safe global variables.
+lazy_static! {
+	static ref OPT: Opt = Opt::from_args();
+}
 
+static PRINT_HEADER: AtomicBool = AtomicBool::new(false);
+
+const NGX_FC_HEADER_VERSION: ngx_uint_t = 5;
 
 // nginx bindings
 type u_char = std::os::raw::c_uchar;
@@ -20,79 +40,510 @@ type u_short = std::os::raw::c_ushort;
 type time_t = std::os::raw::c_long;
 type ngx_uint_t = usize;
 
+#[derive(Debug)]
 #[repr(C)]
 struct ngx_http_file_cache_header_t {
-    version: ngx_uint_t,
-    valid_sec: time_t,
-    updating_sec: time_t,
-    error_sec: time_t,
-    last_modified: time_t,
-    date: time_t,
-    crc32: u32,
-    valid_msec: u_short,
-    header_start: u_short,
-    body_start: u_short,
-    etag_len: u_char,
-    etag: [u_char; 128usize],
-    vary_len: u_char,
-    vary: [u_char; 128usize],
-    variant: [u_char; 16usize],
+	version: ngx_uint_t,
+	valid_sec: time_t,
+	updating_sec: time_t,
+	error_sec: time_t,
+	last_modified: time_t,
+	date: time_t,
+	crc32: u32,
+	valid_msec: u_short,
+	header_start: u_short,
+	body_start: u_short,
+	etag_len: u_char,
+	etag: [u_char; 128_usize],
+	vary_len: u_char,
+	vary: [u_char; 128_usize],
+	variant: [u_char; 16_usize],
 }
 
-
-fn nginx_file_cache_read(file: &Path) {
-    let mut chd: ngx_http_file_cache_header_t = unsafe { mem::zeroed() };
-    let chd_size = mem::size_of::<ngx_http_file_cache_header_t>();
-
-    let mut fd = fs::File::open(file).unwrap();
-
-    unsafe {
-        let chd_slice = slice::from_raw_parts_mut(&mut chd as *mut _ as *mut u8, chd_size);
-        fd.read_exact(chd_slice).unwrap();
-    }
-
-    if chd.version != NGX_FC_HEADER_VERSION {
-        panic!("FATAL: {} header version mismatch. Expected {}, got {}.",
-            file.to_str().unwrap(), NGX_FC_HEADER_VERSION, chd.version);
-    }
-
-    if OPT.debug {
-        println!("{} {} {} {}", file.to_str().unwrap(), chd.valid_sec, chd.updating_sec, chd.error_sec);
-    }
+struct CacheMeta {
+	path: String,
+	cache_header: ngx_http_file_cache_header_t,
+	key: Option<String>,
 }
 
-
-fn path_walk(dir: &Path) {
-    for result in dir.read_dir().unwrap() {
-        if let Ok(entry) = result {
-            let path = entry.path();
-            let file_type = entry.file_type().unwrap();
-            if file_type.is_dir() {
-                path_walk(&path);
-            } else if file_type.is_file() {
-                nginx_file_cache_read(&path);
-            } else {
-                panic!("ERROR: {:?} is neither file nor directory.", path);
-            }
-        }
-    }
+#[derive(PartialEq, Debug)]
+enum CacheStatus {
+	Hit,
+	Updating,
+	Stale,
+	Expired,
 }
 
+impl fmt::Display for CacheStatus {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			CacheStatus::Hit => "Hit".fmt(f),
+			CacheStatus::Updating => "Updating".fmt(f),
+			CacheStatus::Stale => "Stale".fmt(f),
+			CacheStatus::Expired => "Expired".fmt(f),
+		}
+	}
+}
 
+fn calc_cache_status(now: i64, valid_sec: i64, updating_sec: i64, error_sec: i64) -> CacheStatus {
+	// Expiration fields meaning is as follows (nginx-1.18.0/src/http/ngx_http_upstream.c):
+	//  valid_sec = ngx_time() + n (where n is max-age or s-maxage)
+	//  updating_sec = stale-while-revalidate
+	//  error_sec = stale-while-revalidate || stale-if-error
+	// Validation (nginx-1.18.0/src/http/ngx_http_file_cache.c):
+	//  if (c->valid_sec < now) {
+	//    c->stale_updating = c->valid_sec + c->updating_sec >= now;
+	//    c->stale_error = c->valid_sec + c->error_sec >= now;
+	//  }
+	if valid_sec < now {
+		if valid_sec + updating_sec >= now {
+			CacheStatus::Updating
+		} else if valid_sec + error_sec >= now {
+			CacheStatus::Stale
+		} else {
+			CacheStatus::Expired
+		}
+	} else {
+		CacheStatus::Hit
+	}
+}
+
+fn humanize_time(uts: i64, human: bool) -> String {
+	if human && uts != 0 {
+		Local.timestamp(uts, 0).format("%Y-%m-%dT%H:%M:%S").to_string()
+	} else {
+		uts.to_string()
+	}
+}
+
+fn cache_meta_print(cache_meta: &CacheMeta, status: &CacheStatus) {
+	let path = &cache_meta.path;
+	let cache_header = &cache_meta.cache_header;
+	let key = &cache_meta.key;
+
+	let valid = humanize_time(cache_header.valid_sec, OPT.human_time);
+	let updating = if cache_header.updating_sec > 0 {
+		humanize_time(cache_header.valid_sec + cache_header.updating_sec, OPT.human_time)
+	} else {
+		cache_header.updating_sec.to_string()
+	};
+	let error = if cache_header.error_sec > 0 {
+		humanize_time(cache_header.valid_sec + cache_header.error_sec, OPT.human_time)
+	} else {
+		cache_header.error_sec.to_string()
+	};
+
+	let mut header = format!(
+		"{:55}  {:>19}  {:>19}  {:>19}  {:>7}",
+		"path", "expires", "if-updating", "if-error", "status"
+	);
+	if key.is_some() {
+		header = format!("{} {}", header, "key");
+	}
+
+	// Print header only once.
+	if !OPT.no_header && !PRINT_HEADER.load(Ordering::Relaxed) {
+		println!("{}", header);
+		PRINT_HEADER.store(true, Ordering::Relaxed);
+	}
+
+	match key {
+		Some(key) => println!(
+			"{:55}  {:>19}  {:>19}  {:>19}  {:>8}  {}",
+			path,
+			valid,
+			updating,
+			error,
+			status,
+			key.trim().trim_start_matches("KEY: ")
+		),
+		None => println!(
+			"{:55}  {:>19}  {:>19}  {:>19}  {:>8}",
+			path, valid, updating, error, status
+		),
+	}
+}
+
+fn is_match(cache_meta: &CacheMeta, time_range: (Option<i64>, Option<i64>), key_re: Option<&String>) -> bool {
+	let (after_ts, before_ts) = time_range;
+
+	let cache_header = &cache_meta.cache_header;
+
+	let max_valid =
+		(cache_header.valid_sec + cache_header.updating_sec).max(cache_header.valid_sec + cache_header.error_sec);
+
+	if let Some(before_ts) = before_ts {
+		if max_valid > before_ts {
+			return false;
+		}
+	}
+
+	if let Some(after_ts) = after_ts {
+		if max_valid < after_ts {
+			return false;
+		}
+	}
+
+	if let Some(key_re) = key_re {
+		if let Some(key) = &cache_meta.key {
+			if !key.contains(key_re) {
+				return false;
+			}
+		}
+	}
+
+	true
+}
+
+fn nginx_file_cache_read(path: &Path, read_key: bool) -> Result<CacheMeta, Box<dyn error::Error>> {
+	let display = path.display();
+	let mut cache_header: ngx_http_file_cache_header_t = unsafe { mem::zeroed() };
+	let cache_header_size = mem::size_of::<ngx_http_file_cache_header_t>();
+
+	let mut file = match fs::File::open(path) {
+		Err(why) => panic!("couldn't open {}: {}", display, why),
+		Ok(file) => file,
+	};
+
+	#[allow(clippy::semicolon_if_nothing_returned, clippy::ptr_as_ptr)]
+	unsafe {
+		// std::io::read_exact() expects a reference to mutable u8 buffer with known length.
+		// slice is Rust’s solution to (raw) pointer+length.
+		// In the end, cache_header and cache_header_slice point to the same memory.
+		let cache_header_slice = slice::from_raw_parts_mut(&mut cache_header as *mut _ as *mut u8, cache_header_size);
+		file.read_exact(cache_header_slice)?
+	};
+
+	if cache_header.version != NGX_FC_HEADER_VERSION {
+		panic!(
+			"FATAL: {} header version mismatch. Expected {}, got {}.",
+			display, NGX_FC_HEADER_VERSION, cache_header.version
+		);
+	}
+
+	let max_key_size = 8190; // apache's LimitRequestLine
+	let min_header_start = cache_header_size + b"\nKEY: C\n".len();
+	if (cache_header.header_start as usize) < min_header_start || (cache_header.header_start as usize) > max_key_size {
+		panic!("FATAL: {} invalid header_start {}", display, cache_header.header_start);
+	}
+
+	let key = if read_key {
+		let mut buf = vec![0_u8; cache_header.header_start as usize - cache_header_size];
+		file.read_exact(&mut buf)?;
+
+		let key = str::from_utf8(&buf)?;
+		let key = key.strip_prefix("\nKEY: ");
+
+		if key.is_none() {
+			return Err("missing key label".into());
+		}
+
+		Some(key.unwrap().to_string())
+	} else {
+		Option::None
+	};
+
+	if OPT.debug {
+		eprintln!("D: path {}, key {:?}", display, key);
+	}
+
+	Ok(CacheMeta {
+		path: path.to_str().unwrap().to_string(),
+		cache_header,
+		key,
+	})
+}
+
+fn purge(path: &Path) {
+	if OPT.debug {
+		eprintln!("unlink() {}", path.display());
+	}
+
+	if let Err(e) = fs::remove_file(path) {
+		if !OPT.hide_unlink_errors {
+			eprintln!("purge({}) failed: {:?}", path.display(), e);
+		}
+	}
+}
+
+fn path_walk(dir: &Path, time_range: (Option<i64>, Option<i64>)) {
+	let rd_itr = match dir.read_dir() {
+		Ok(rd_itr) => rd_itr,
+		Err(e) => panic!("{} {:?}", dir.display(), e),
+	};
+
+	for entry in rd_itr.flatten() {
+		let path = entry.path();
+		let file_type = entry.file_type().unwrap();
+
+		if file_type.is_dir() {
+			path_walk(&path, time_range);
+		} else if file_type.is_file() {
+			let cache_meta = match nginx_file_cache_read(&path, OPT.match_key.is_some()) {
+				Ok(cm) => cm,
+				Err(e) => {
+					eprintln!("nginx_file_cache_read({}) error: {:?}", path.display(), e);
+					continue;
+				}
+			};
+
+			if !is_match(&cache_meta, time_range, OPT.match_key.as_ref()) {
+				continue;
+			}
+
+			if !OPT.quiet {
+				let uts = SystemTime::now()
+					.duration_since(SystemTime::UNIX_EPOCH)
+					.unwrap()
+					.as_secs();
+				let uts = i64::try_from(uts).expect("SystemTime::now() u64 -> i64 overflow?");
+				let status = calc_cache_status(
+					uts,
+					cache_meta.cache_header.valid_sec,
+					cache_meta.cache_header.updating_sec,
+					cache_meta.cache_header.error_sec,
+				);
+				cache_meta_print(&cache_meta, &status);
+			}
+
+			if OPT.purge {
+				purge(path.as_path());
+			}
+		} else {
+			panic!("ERROR: {:?} is neither file nor directory.", path);
+		}
+	}
+}
+
+// structopt will be merged in clap 3.x soon
+#[allow(clippy::struct_excessive_bools)]
 #[derive(StructOpt, Debug)]
 struct Opt {
-    cache_dir: String,
-    /// Activate debug mode
-    #[structopt(short = "d", long = "debug")]
-    debug: bool,
-}
+	cache_dir: String,
 
-lazy_static! {
-    static ref OPT: Opt = Opt::from_args();
+	#[structopt(long = "debug", short = "d")]
+	debug: bool,
+
+	#[structopt(long = "quiet", short = "q")]
+	quiet: bool,
+
+	#[structopt(long = "hide-unlink-errors")]
+	hide_unlink_errors: bool,
+
+	#[structopt(long = "no-header")]
+	no_header: bool,
+
+	#[structopt(long = "human-time")]
+	human_time: bool,
+
+	#[structopt(long = "expires-after", short = "a")]
+	expires_after: Option<String>,
+
+	/// Default is now() if no --expires-after and --expires-before were given; check with --debug
+	#[structopt(long = "expires-before", short = "b")]
+	expires_before: Option<String>,
+
+	/// Usually key is an URL. Non-regex substring match.
+	#[structopt(long = "match-key")]
+	match_key: Option<String>,
+
+	/// Purge ALL matched cache files (not limited to Expired)
+	#[structopt(long = "purge")]
+	purge: bool,
 }
 
 pub fn main() {
-    let path = Path::new(&OPT.cache_dir);
-    path_walk(path);
+	let expires_before: Option<String>;
+
+	let mut exp_aft_ts: Option<i64> = Option::None;
+	let mut exp_bef_ts: Option<i64> = Option::None;
+
+	if OPT.expires_after.is_none() && OPT.expires_before.is_none() {
+		expires_before = Option::Some("now".to_string());
+	} else {
+		expires_before = OPT.expires_before.clone();
+	}
+
+	if let Some(tmp_after) = &OPT.expires_after {
+		let exp_a_dt = parse_date_string(tmp_after, Local::now(), Dialect::Uk).unwrap();
+		if OPT.debug {
+			eprintln!("After: {}", exp_a_dt);
+		}
+		exp_aft_ts = Option::Some(exp_a_dt.timestamp());
+	}
+
+	if let Some(tmp_before) = expires_before {
+		let exp_b_dt = parse_date_string(&tmp_before, Local::now(), Dialect::Uk).unwrap();
+		if OPT.debug {
+			eprintln!("Before: {}", exp_b_dt);
+		}
+		exp_bef_ts = Option::Some(exp_b_dt.timestamp());
+	}
+
+	let time_range = (exp_aft_ts, exp_bef_ts);
+	let path = Path::new(&OPT.cache_dir);
+
+	if OPT.debug && OPT.match_key.is_some() {
+		eprintln!("Match: {}", OPT.match_key.as_ref().unwrap());
+	}
+
+	if let Some(a) = exp_aft_ts {
+		if let Some(b) = exp_bef_ts {
+			if b < a {
+				panic!("Conflicting expires options: before < after ({} < {}).", b, a);
+			}
+		}
+	}
+
+	path_walk(path, time_range);
 }
 
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use proptest::prelude::*;
+
+	#[test]
+	fn test_calc_cache_status() {
+		let now = 100;
+		assert_eq!(calc_cache_status(now, 101, 0, 0), CacheStatus::Hit);
+		assert_eq!(calc_cache_status(now, 100, 0, 0), CacheStatus::Hit);
+		assert_eq!(calc_cache_status(now, 100, 1, 1), CacheStatus::Hit);
+
+		assert_eq!(calc_cache_status(now, 99, 1, 0), CacheStatus::Updating);
+		assert_eq!(calc_cache_status(now, 99, 1, 1), CacheStatus::Updating);
+
+		assert_eq!(calc_cache_status(now, 99, 0, 1), CacheStatus::Stale);
+
+		assert_eq!(calc_cache_status(now, 99, 0, 0), CacheStatus::Expired);
+		assert_eq!(calc_cache_status(now, 98, 0, 0), CacheStatus::Expired);
+		assert_eq!(calc_cache_status(now, 98, 1, 0), CacheStatus::Expired);
+		assert_eq!(calc_cache_status(now, 98, 0, 1), CacheStatus::Expired);
+		assert_eq!(calc_cache_status(now, 98, 1, 1), CacheStatus::Expired);
+	}
+
+	#[test]
+	fn test_humanize_time() {
+		assert_eq!(humanize_time(100, false), "100");
+		assert_eq!(humanize_time(100, true), "1970-01-01T02:01:40");
+		assert_eq!(humanize_time(0, false), "0");
+		assert_eq!(humanize_time(0, true), "0");
+	}
+
+	#[test]
+	fn test_is_match() {
+		let cache_header: ngx_http_file_cache_header_t = unsafe { mem::zeroed() };
+		let path = "/var/nginx/cache/a/b/deadbeef".to_string();
+		let key = Option::None;
+		let mut cm = CacheMeta {
+			path,
+			cache_header,
+			key,
+		};
+
+		// valid_sec alone
+		cm.cache_header.valid_sec = 150;
+		assert!(
+			is_match(&cm, (Some(100), Some(200)), Option::None),
+			"expiration is within filter limits"
+		);
+
+		cm.cache_header.valid_sec = 100;
+		assert!(
+			is_match(&cm, (Some(100), Some(200)), Option::None),
+			"expiration is within filter limits (= after)"
+		);
+
+		cm.cache_header.valid_sec = 200;
+		assert!(
+			is_match(&cm, (Some(100), Some(200)), Option::None),
+			"expiration is within filter limits (= before)"
+		);
+
+		cm.cache_header.valid_sec = 50;
+		assert!(
+			!is_match(&cm, (Some(100), Some(200)), Option::None),
+			"expiration is outside filter limits (< after)"
+		);
+
+		cm.cache_header.valid_sec = 250;
+		assert!(
+			!is_match(&cm, (Some(100), Some(200)), Option::None),
+			"expiration outside filter limits (> before)"
+		);
+
+		cm.cache_header.valid_sec = 50;
+
+		cm.cache_header.updating_sec = 50;
+		assert!(
+			is_match(&cm, (Some(100), Some(200)), Option::None),
+			"expiration/updating is within filter limits (=after, <before)"
+		);
+		cm.cache_header.updating_sec = 49;
+		assert!(
+			!is_match(&cm, (Some(100), Some(200)), Option::None),
+			"expiration/updating is outside filter limits (<after)"
+		);
+
+		cm.cache_header.updating_sec = 0;
+
+		cm.cache_header.error_sec = 150;
+		assert!(
+			is_match(&cm, (Some(100), Some(200)), Option::None),
+			"expiration/error is within filter limits (>after, =before)"
+		);
+		cm.cache_header.error_sec = 151;
+		assert!(
+			!is_match(&cm, (Some(100), Some(200)), Option::None),
+			"expiration/error is outside filter limits (>after, >before)"
+		);
+
+		// fix valid_sec for key match tests
+		cm.cache_header.valid_sec = 150;
+		cm.cache_header.updating_sec = 0;
+		cm.cache_header.error_sec = 0;
+
+		cm.key = Some("http://example.com/white".to_string());
+		assert!(
+			is_match(&cm, (Some(100), Some(200)), Some(&"white".to_string())),
+			"key matches"
+		);
+		assert!(
+			!is_match(&cm, (Some(100), Some(200)), Some(&"black".to_string())),
+			"key doesn't match"
+		);
+	}
+
+	proptest! {
+		#[test]
+		fn match_any_printable_key_string(s in ".*") {
+			let cache_header: ngx_http_file_cache_header_t = unsafe { mem::zeroed() };
+			let path = "/var/nginx/cache/a/b/deadbeef".to_string();
+			let key = Some(s.clone());
+			let mut cm = CacheMeta {path, cache_header, key};
+
+			cm.cache_header.valid_sec = 150;
+			cm.cache_header.updating_sec = 0;
+			cm.cache_header.error_sec = 0;
+
+			prop_assert_eq!(true, is_match(&cm, (Some(100), Some(200)), Some(&s)), "random data");
+		}
+
+		#[test]
+		fn match_corrupted_data_no_crash(s in ".{336}") {
+			let path = "/var/nginx/cache/a/b/deadbeef".to_string();
+
+			let mut cache_header: ngx_http_file_cache_header_t = unsafe { mem::zeroed() };
+			let cache_header_size = mem::size_of::<ngx_http_file_cache_header_t>();
+			let cache_header_slice = unsafe { slice::from_raw_parts_mut(&mut cache_header as *mut _ as *mut u8, cache_header_size) };
+
+			cache_header_slice.copy_from_slice(&s.as_bytes()[1..=336]);
+
+			let key = Some(s);
+
+			let cm = CacheMeta {path, cache_header, key};
+
+			prop_assert_eq!(false, is_match(&cm, (Some(100), Some(200)), Some(&"x".to_string())), "random data");
+		}
+	}
+}
---
+++ tests/cli.rs
@@ -0,0 +1,256 @@
+#![allow(non_camel_case_types)]
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::convert::TryInto;
+use std::fs;
+use std::io::{ErrorKind, Write};
+use std::mem;
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
+use std::process::Command;
+use std::slice;
+
+// nginx bindings
+type u_char = std::os::raw::c_uchar;
+type u_short = std::os::raw::c_ushort;
+type time_t = std::os::raw::c_long;
+type ngx_uint_t = usize;
+
+// #[derive(Debug)] // Requires rust 1.47 due to std::array::LengthAtMost32
+#[repr(C)]
+struct ngx_http_file_cache_header_t {
+	version: ngx_uint_t,
+	valid_sec: time_t,
+	updating_sec: time_t,
+	error_sec: time_t,
+	last_modified: time_t,
+	date: time_t,
+	crc32: u32,
+	valid_msec: u_short,
+	header_start: u_short,
+	body_start: u_short,
+	etag_len: u_char,
+	etag: [u_char; 128_usize],
+	vary_len: u_char,
+	vary: [u_char; 128_usize],
+	variant: [u_char; 16_usize],
+}
+
+const CLI_NAME: &str = "nginx-ecm-rs";
+
+fn create_cache_file(
+	tmp_dir: &std::path::Path,
+	version: usize,
+	key: Option<&[u8]>,
+) -> Result<std::path::PathBuf, Box<dyn std::error::Error>> {
+	let mut file_path = tmp_dir.to_path_buf();
+	let mut file = Option::None;
+
+	// Create unique file if called multiple times from single test (3 attempts)
+	for i in 1..=3 {
+		file_path = tmp_dir.join(format!("cache-file-{}", i));
+		file = match fs::OpenOptions::new().write(true).create_new(true).open(&file_path) {
+			Ok(f) => Some(f),
+			Err(e) => {
+				if e.kind() == ErrorKind::AlreadyExists {
+					continue;
+				}
+				panic!("{:#?}", e);
+			}
+		};
+		if file.is_some() {
+			break;
+		}
+	}
+	let mut file = file.unwrap();
+
+	let key = match key {
+		Some(key) => key,
+		None => b"\nKEY: .",
+	};
+
+	let mut cache_header: ngx_http_file_cache_header_t = unsafe { mem::zeroed() };
+	let cache_header_size = mem::size_of::<ngx_http_file_cache_header_t>();
+
+	cache_header.version = version;
+	cache_header.header_start = (cache_header_size + key.len()).try_into().unwrap();
+
+	unsafe {
+		let cache_header_slice = slice::from_raw_parts_mut(&mut cache_header as *mut _ as *mut u8, cache_header_size);
+		file.write_all(cache_header_slice)?;
+	}
+	file.write_all(key)?;
+
+	Ok(file_path)
+}
+
+#[test]
+fn cache_dir_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+	let mut cmd = Command::cargo_bin("nginx-ecm-rs")?;
+	cmd.arg("/no/such/dir/");
+	cmd.assert()
+		.failure()
+		.stderr(predicate::str::contains("No such file or directory"));
+
+	Ok(())
+}
+
+#[test]
+fn cache_dir_no_permission() -> Result<(), Box<dyn std::error::Error>> {
+	let dir = tempfile::tempdir().unwrap();
+	let path = dir.path();
+
+	let metadata = path.metadata()?;
+	let mut permissions = metadata.permissions();
+	permissions.set_mode(0o000);
+	fs::set_permissions(path, permissions)?;
+
+	let mut cmd = Command::cargo_bin("nginx-ecm-rs")?;
+	cmd.arg(path);
+	cmd.assert()
+		.failure()
+		.stderr(predicate::str::contains("Permission denied"));
+
+	Ok(())
+}
+
+#[test]
+fn special_file() -> Result<(), Box<dyn std::error::Error>> {
+	let dir = tempfile::tempdir()?;
+
+	Command::new("mkfifo").arg(dir.path().join("fifo")).status().unwrap();
+
+	let mut cmd = Command::cargo_bin(CLI_NAME)?;
+	cmd.arg(dir.path());
+	cmd.assert()
+		.failure()
+		.stderr(predicate::str::contains("neither file nor directory"));
+
+	Ok(())
+}
+
+#[test]
+fn cache_file_no_permission() -> Result<(), Box<dyn std::error::Error>> {
+	let dir = tempfile::tempdir().unwrap();
+	let path = dir.path();
+
+	let file_path = dir.path().join("cache-test.bin");
+	fs::File::create(&file_path)?;
+
+	let metadata = file_path.metadata()?;
+	let mut permissions = metadata.permissions();
+	permissions.set_mode(0o000);
+	fs::set_permissions(file_path, permissions)?;
+
+	let mut cmd = Command::cargo_bin("nginx-ecm-rs")?;
+	cmd.arg(path);
+	cmd.assert()
+		.failure()
+		.stderr(predicate::str::contains("Permission denied"));
+
+	Ok(())
+}
+
+#[test]
+fn short_cache_file() -> Result<(), Box<dyn std::error::Error>> {
+	let dir = tempfile::tempdir()?;
+	let file_path = dir.path().join("cache-test.bin");
+	let mut file = fs::File::create(&file_path)?;
+
+	writeln!(file, ".")?;
+
+	let mut cmd = Command::cargo_bin(CLI_NAME)?;
+	cmd.arg(dir.path());
+	cmd.assert()
+		.success()
+		.stderr(predicate::str::contains("failed to fill whole buffer"));
+
+	Ok(())
+}
+
+#[test]
+fn valid_nginx_cache_file() -> Result<(), Box<dyn std::error::Error>> {
+	let dir_path = Path::new("tests/data");
+
+	let mut cmd = Command::cargo_bin(CLI_NAME)?;
+	cmd.arg(dir_path).arg("--match-key").arg("zimage.example.com");
+	cmd.assert()
+		.success()
+		.stdout(predicate::str::contains("77cefbbd3b90b3f68899b6c7aa02d007"));
+
+	Ok(())
+}
+
+#[test]
+fn valid_generated_cache_file() -> Result<(), Box<dyn std::error::Error>> {
+	let tmp_dir = tempfile::tempdir()?;
+	create_cache_file(tmp_dir.path(), 5, Some(b"\nKEY: http://example.com/hello.html"))?;
+
+	let mut cmd = Command::cargo_bin(CLI_NAME)?;
+	cmd.arg(tmp_dir.path()).arg("--match-key").arg("example.com");
+	cmd.assert().success().stdout(predicate::str::contains("example"));
+
+	Ok(())
+}
+
+#[test]
+fn invalid_cache_header_version() -> Result<(), Box<dyn std::error::Error>> {
+	let tmp_dir = tempfile::tempdir()?;
+	create_cache_file(tmp_dir.path(), 6, None)?;
+
+	let mut cmd = Command::cargo_bin(CLI_NAME)?;
+	cmd.arg(tmp_dir.path());
+	cmd.assert()
+		.failure()
+		.stderr(predicate::str::contains("header version mismatch"));
+
+	Ok(())
+}
+
+#[test]
+fn invalid_key_string() -> Result<(), Box<dyn std::error::Error>> {
+	let tmp_dir = tempfile::tempdir()?;
+
+	// 0xfe is invalid utf9 sequence
+	//                             \n     K     E     Y     :    \s
+	let bytes: &'static [u8] = &[0x0a, 0x4b, 0x45, 0x59, 0x3a, 0x20, 0xfe, 0x0a];
+	let key = Some(bytes);
+	let file_path = create_cache_file(tmp_dir.path(), 5, key)?;
+
+	let mut cmd = Command::cargo_bin(CLI_NAME)?;
+	cmd.arg(tmp_dir.path()).arg("--match-key").arg("example.com");
+
+	let res = cmd.assert().success().try_stderr(predicate::str::contains("Utf8Error"));
+
+	if let Err(e) = res {
+		let bad_file = tempfile::NamedTempFile::new().unwrap();
+		let bad_path = bad_file.into_temp_path();
+		let bad_path = bad_path.keep()?;
+		fs::copy(&file_path, &bad_path).unwrap();
+		panic!("Test file preserved as {} due to {}", bad_path.display(), e);
+	}
+
+	Ok(())
+}
+
+#[test]
+fn purge_purges_only_matched() -> Result<(), Box<dyn std::error::Error>> {
+	let tmp_dir = tempfile::tempdir()?;
+	let tmp_dir = tmp_dir.path();
+
+	let fcom = create_cache_file(tmp_dir, 5, Some(b"\nKEY: http://example.com/hello.html"))?;
+	let forg = create_cache_file(tmp_dir, 5, Some(b"\nKEY: http://example.org/hello.html"))?;
+
+	assert!(fcom.exists());
+	assert!(forg.exists());
+
+	let mut cmd = Command::cargo_bin(CLI_NAME)?;
+	cmd.arg(tmp_dir).arg("--match-key").arg("example.com").arg("--purge");
+	cmd.assert().success().stdout(predicate::str::contains("example.com"));
+
+	assert!(!fcom.exists());
+	assert!(forg.exists());
+
+	Ok(())
+}
